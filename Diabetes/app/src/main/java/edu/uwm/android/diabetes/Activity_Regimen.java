c45d8e2167f4e9ebb00630551b0e714a6de7234f
Nate <jamesonn@uwm.edu>
2017-07-09 21:55:39 -0500

    merging in musaed's changes

---
+++ Diabetes/app/src/main/java/edu/uwm/android/diabetes/Activity_Regimen.java
@@ -0,0 +1,13 @@
+package edu.uwm.android.diabetes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Regimen extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__regimen);
+    }
+}
