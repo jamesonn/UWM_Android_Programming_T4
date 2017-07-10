f6ad5379d3ffbc8f77bb49d787251b618f8d37d1
Nate <jamesonn@uwm.edu>
2017-07-09 22:43:08 -0500

    styles and new activities

---
+++ Diabetes/app/src/main/java/edu/uwm/android/diabetes/RemindersActivity.java
@@ -0,0 +1,13 @@
+package edu.uwm.android.diabetes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemindersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+    }
+}
