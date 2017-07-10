9b07b249a01c0a99f438b7be61f22f6c1e73e26a
Nate <jamesonn@uwm.edu>
2017-07-09 19:11:57 -0500

    Unzipped project; compile and target sdk changed to 23

---
+++ Diabetes/app/src/main/java/edu/uwm/android/diabetes/SplashActivity.java
@@ -0,0 +1,35 @@
+package edu.uwm.android.diabetes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Rafa on 7/3/2017.
+ */
+
+public class SplashActivity extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splashscreen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
