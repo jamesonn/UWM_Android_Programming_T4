3b120b1f6e10f043822d9ffc79fc76facd5cf565
Nate <jamesonn@uwm.edu>
2017-07-11 20:03:14 -0500

    replacing deprecated method

--- Diabetes/app/src/main/java/edu/uwm/android/diabetes/MainActivity.java
+++ Diabetes/app/src/main/java/edu/uwm/android/diabetes/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -90,10 +90,12 @@ public class MainActivity extends AppCompatActivity
             intent = new Intent(this, RegimenActivity.class );
         }
 
+        startActivity(intent);
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
 
-        startActivity(intent);
+
         return true;
     }
 }
