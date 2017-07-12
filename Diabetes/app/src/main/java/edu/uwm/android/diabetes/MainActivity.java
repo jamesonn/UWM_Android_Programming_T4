66187929562cdd4212fdb9be774f56ef5ef21336
Nate <jamesonn@uwm.edu>
2017-07-11 19:00:06 -0500

    test commit

--- Diabetes/app/src/main/java/edu/uwm/android/diabetes/MainActivity.java
+++ Diabetes/app/src/main/java/edu/uwm/android/diabetes/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+//test commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
